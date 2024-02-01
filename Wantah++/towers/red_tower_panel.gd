b508ee049d8b2f1b5e1a4c19284df9e52cb830b4
AKARandy <mcpe490@gmail.com>
2024-02-01 17:21:08 +0700

    GUI bisa drag, dan gak rusak fullscreenya
    ganti mode screennya juga

--- Wantah++/towers/red_tower_panel.gd
+++ Wantah++/towers/red_tower_panel.gd
@@ -3,12 +3,34 @@ extends Panel
 
 @onready var tower = preload("res://towers/red_tower.tscn")
 var currTile
+var tempPos
 
 func _on_gui_input(event):
 	var tempTower = tower.instantiate()
 	if event is InputEventMouseButton and event.button_mask == 1:
-		print("left click down")
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		#print("left click down")
+		
 	elif event is InputEventMouseMotion and event.button_mask == 1:
-		print("left click down drag")
+		if get_child_count(1) > 1:
+			get_child(1).global_position = event.global_position
+			tempPos = event.global_position
+		print(event.global_position)
+		#print("left click down drag")
+		
 	elif event is InputEventMouseButton and event.button_mask == 0:
-		print("left click up")
+		if get_child_count(1) > 1:
+			get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Main/Towers")
+		path.add_child(tempTower)
+		tempTower.global_position = tempPos
+		tempTower.get_node("Area").hide()
+		if tempTower.global_position.x > 2112:
+			tempTower.queue_free()
+		print(tempPos)
+		#print("left click up")
+		
+	else:
+		if get_child_count(1) > 1:
+			get_child(1).queue_free()
