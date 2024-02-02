a4a780d51894bdd8b88205a97100818961a53802
AKARandy <mcpe490@gmail.com>
2024-02-02 16:29:01 +0700

    Gak bisa naro tower di jalanan dan ada fitur UI untuk fitur upgrade ke depan

--- Wantah++/towers/red_tower_panel.gd
+++ Wantah++/towers/red_tower_panel.gd
@@ -16,20 +16,32 @@ func _on_gui_input(event):
 		if get_child_count(1) > 1:
 			get_child(1).global_position = event.global_position
 			tempPos = event.global_position
+			
+		var mapPath = get_tree().get_root().get_node("Main/TileMap")
+		var tile = mapPath.local_to_map(get_global_mouse_position())
+		currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+		if currTile != Vector2i(4, 5):
+			get_child(1).get_node("Area").modulate = Color(200 ,0, 0, 0.5)
+		if currTile == Vector2i(4, 5):
+			get_child(1).get_node("Area").modulate = Color(0 ,0, 0, 0.314)
+			
 		print(event.global_position)
 		#print("left click down drag")
 		
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		if get_child_count(1) > 1:
 			get_child(1).queue_free()
+			
 		var path = get_tree().get_root().get_node("Main/Towers")
 		path.add_child(tempTower)
 		tempTower.global_position = tempPos
 		tempTower.get_node("Area").hide()
-		if tempTower.global_position.x > 2112:
+		if tempTower.global_position.x > 2112 or currTile != Vector2i(4, 5):
 			tempTower.queue_free()
+		
+			
 		print(tempPos)
-		#print("left click up")
+		#print("left click up") 
 		
 	else:
 		if get_child_count(1) > 1:
