7620590181dacbffc70417bd1a1819a1c7103e67
AKARandy <mcpe490@gmail.com>
2024-02-03 22:35:26 +0700

    Ganti map

--- Wantah++/towers/red_tower_panel.gd
+++ Wantah++/towers/red_tower_panel.gd
@@ -18,13 +18,13 @@ func _on_gui_input(event):
 			get_child(1).global_position = event.global_position
 			tempPos = event.global_position
 			
-		var mapPath = get_tree().get_root().get_node("Main/TileMap")
-		var tile = mapPath.local_to_map(get_global_mouse_position())
-		currTile = mapPath.get_cell_atlas_coords(0, tile, false)
-		if currTile != Vector2i(4, 5):
-			get_child(1).get_node("Area").modulate = Color(200 ,0, 0, 0.5)
-		if currTile == Vector2i(4, 5):
-			get_child(1).get_node("Area").modulate = Color(0 ,0, 0, 0.314)
+		#var mapPath = get_tree().get_root().get_node("Main/TileMap")
+		#var tile = mapPath.local_to_map(get_global_mouse_position())
+		#currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+		#if currTile != Vector2i(4, 5):
+		#	get_child(1).get_node("Area").modulate = Color(200 ,0, 0, 0.5)
+		#if currTile == Vector2i(4, 5):
+		#	get_child(1).get_node("Area").modulate = Color(0 ,0, 0, 0.314)
 			
 		print(event.global_position)
 		#print("left click down drag")
@@ -37,7 +37,7 @@ func _on_gui_input(event):
 		path.add_child(tempTower)
 		tempTower.global_position = tempPos
 		tempTower.get_node("Area").hide()
-		if tempTower.global_position.x > 2112 or currTile != Vector2i(4, 5):
+		if tempTower.global_position.x > 2112:
 			tempTower.queue_free()
 		
 			
